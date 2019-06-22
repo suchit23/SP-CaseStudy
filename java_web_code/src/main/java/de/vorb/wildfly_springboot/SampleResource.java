e1b48595c4b33fe96dd3d0869b6e8f2dea5bb2fb
suchit23 <suchit23@gmail.com>
2019-06-23 00:30:37 +0530

    Cheked in after some minor changes

--- java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
+++ java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
 
 @RestController
 public class SampleResource {
