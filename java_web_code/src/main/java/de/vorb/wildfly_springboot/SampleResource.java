f97b612b1ca1bf1bfc58d9cb1e07a745ff095780
suchit23 <suchit23@gmail.com>
2019-06-22 12:53:24 +0530

    Added yaml files

--- java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
+++ java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
@@ -2,13 +2,37 @@ package de.vorb.wildfly_springboot;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @RestController
 public class SampleResource {
 
     @RequestMapping("/")
     public String hello() {
-        return "Hello Suchit's World! ----- Version 3333";
+    	try {
+            String parseLine; /* variable definition *//* create objects */            URL URL = new URL("http://www.example.com/"); 
+            StringBuffer sbrDoc = new StringBuffer();
+            BufferedReader br = new BufferedReader(new InputStreamReader(URL.openStream()));
+
+            while ((parseLine = br.readLine()) != null) {
+                System.out.println(parseLine);
+                sbrDoc.append(parseLine);
+            }
+            br.close();
+
+        } catch (MalformedURLException me) {
+            System.out.println(me);
+
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+        String strBuildSystem = "Hello Suchit's World! ----- Version 3333";
+        String strReturn = sbrDoc + strBuildSystem;
+        return strReturn;
     }
 
 }
