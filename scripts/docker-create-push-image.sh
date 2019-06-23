2adf708be43770b7215d565110e0cb4b98e601a4
suchit23 <suchit23@gmail.com>
2019-06-23 21:27:43 +0530

    Cheked in after some minor changes

--- scripts/docker-create-push-image.sh
+++ scripts/docker-create-push-image.sh
@@ -11,6 +11,6 @@ sudo docker build -t suchit23/sp-casestudy-docker-image .
 echo "******* :: Starting Docker Push Tasks :: *******"
 echo ""
 
-sudo cat /root/my_password.txt | docker login --username suchit23 --password-stdin
+sudo cat /root/my_password.txt | sudo docker login --username suchit23 --password-stdin
 sudo docker push suchit23/sp-casestudy-docker-image
 cd ..
