d7d18878b8179ef0335e6b3f76ad718312651c8d
suchit23 <suchit23@gmail.com>
2019-06-23 13:18:21 +0530

    Cheked in after some minor changes

--- java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
+++ java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
@@ -20,43 +20,18 @@ public class SampleResource {
     @RequestMapping("/")
     public String hello() throws Exception {
 
-            String parseLine; /* variable definition *//* create objects */
-            //StringBuffer sbrDia = new StringBuffer();
-            String strDia = "<br><img src=/spdevops.jpg><br>";
-            StringBuffer sbrDoc = new StringBuffer();
-            String strHello = "<h1>Hello and welcome to Suchit's CI/CD Pipeline Demo! ----- Version 4.0</h1><br>";
+            String parseLine; 
+			String strHello = "<h1>Hello and welcome to Suchit's CI/CD Pipeline Demo! ----- Version 4.0</h1><br>";
+            String strDia = "<br><h2>High Level Architecture Diagram</h2><br><img src=/spdevops.jpg><br>";
+            String strDoc = "<h2><a href=http://blog.suchit23.in/environment-details-access> Step by Step Guide to implement this project></h2><br>"
             StringBuffer sbrBuildSystem = new StringBuffer();
             InetAddress inetAddress = InetAddress.getLocalHost();
             DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
             sdf.setTimeZone(TimeZone.getTimeZone("IST"));
             Date date = new Date();
-            sbrBuildSystem.append("<h2> IP Address where this was built: " + inetAddress.getHostAddress() + "<br> Hostname where this was built: " + inetAddress.getHostName() + "<br> Time when this was built: " + sdf.format(date));
-            
+            sbrBuildSystem.append("<h2> IP Address where this was built: " + inetAddress.getHostAddress() + "<br> Hostname where this was built: " + inetAddress.getHostName() + "<br> Time when this was built: " + sdf.format(date) + "<br>");
 
-            
-            try{
-            //URL url1= new URL("https://drive.google.com/file/d/1GNno3JoIn3YmXVSPcVxI4B7Pm2knuip-/view");
-            URL url2= new URL("http://blog.suchit23.in/environment-details-access/"); 
-            
-
-            /*BufferedReader br1 = new BufferedReader(new InputStreamReader(url1.openStream()));
-            while ((parseLine = br1.readLine()) != null) {
-                System.out.println(parseLine);
-                sbrDia.append(parseLine);
-            	}
-            	br1.close();*/
-
-        	BufferedReader br2 = new BufferedReader(new InputStreamReader(url2.openStream()));
-            while ((parseLine = br2.readLine()) != null) {
-                System.out.println(parseLine);
-                sbrDoc.append(parseLine);
-            	}
-            	br2.close();
-        	}
-        	catch (MalformedURLException me){System.out.println(me);}
-        	catch (IOException ioe){System.out.println(ioe);}
-
-        	String strReturn = strHello + sbrBuildSystem + strDia;
+        	String strReturn = strHello + sbrBuildSystem + strDia + strDoc;
         	return strReturn;
 
     }
